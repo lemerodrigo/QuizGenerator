67bbf74ade2a76c85798b174740daa31bdca4720
Joseph Vu <jvu009@ucr.edu>
2017-02-09 20:01:44 -0800

    fix gitignore

--- server/config/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  url: 'postgres://postgres:password@localhost:5432/databasename'
-};
--- server/models/db.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize');
-const config = require('../config/db');
-
-const db = new Sequelize(config.url);
-
-module.exports = db;
