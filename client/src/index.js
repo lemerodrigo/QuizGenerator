58af9c0c7353d0bf1f12d7bfc92098a559dcfa68
Rodrigo Leme <lemerodrigo@gmail.com>
2017-02-10 10:46:53 -0800

    Bootstrap + Login component

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { Router, IndexRoute, Route, browserHistory } from 'react-router';
 
 import App from './components/App.jsx';
+import Login from './components/Login.jsx';
 import Home from './components/Home.jsx';
 import Quizzes from './components/Quizzes.jsx';
 import NewQuiz from './components/NewQuiz.jsx';
-
+ 
 ReactDOM.render((
   <Router history={browserHistory}>
+    <Route path='login' component={Login} />
     <Route path='/' component={App}>
       <IndexRoute component={Home} />
       <Route path='quizzes'>
